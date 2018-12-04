f55dec1c7d97613e8f6e35e9a7cbf14205655cf1
Bedmar <45603179+Bedmar@users.noreply.github.com>
2018-12-04 23:11:25 +0100

    Update RALSEIBOT.py

--- RALSEIBOT.py
+++ RALSEIBOT.py
@@ -15,7 +15,7 @@ Clientdiscord = discord.Client()
  
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='testbot'))
+    await client.change_presence(game=Game(name='Bedmar V1.0'))
     print('Ready, Freddy') 
  
  
